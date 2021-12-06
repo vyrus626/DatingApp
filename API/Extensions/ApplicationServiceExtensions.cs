4f9d1f822d344427a29dd87524091bc0cab043ba
danny maranan <danny.maranan@gmail.com>
2021-12-06 16:21:36 +0800

    End of Section 15

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -21,8 +21,9 @@ namespace API.Extensions
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<LogUserActivity>();
             services.AddScoped<ILikesRepository, LikesRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
+            services.AddScoped<LogUserActivity>();
             object p = services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options => {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
